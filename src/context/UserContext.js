a1425654d51ab2b7e5fc072481dc7aae534918d0
Marcus Radder <marcusradder@gmail.com>
2021-07-18 05:33:29 -0700

    Cleaned up the authentication forms

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,27 +3,6 @@ import users from './users'
 
 const UserContext = createContext()
 const UserContextProvider = props => {
-    const initUser = {
-        name: "isLoggedOut",
-        password: "password", // for demo purposes only
-        totalScreenTime: 436,
-        totalSitesPerDay: 17,
-        totalDigitalIds: 9,
-        mostTimeSpent: "StackOverflow",
-        moneySpent: 240,
-        actuals: {
-            social: 145,
-            productivity: 45,
-            finance: 345,
-            entertainment: 45,
-        },
-        goals: {
-            social: 45,
-            productivity: 45,
-            finance: 45,
-            entertainment: 45,
-        },
-    }
 
 
     const checkUserGoals2Actual = () => {
